316480238dd36d88acc2186bf1d62dc028e9c3a3
schaeferC <claudia.schaef@gmx.net>
2012-05-29 15:36:28 +0200

    weiterfuehrung pair

--- Pair/main.cpp
+++ Pair/main.cpp
@@ -13,10 +13,19 @@ int main()
     int i = 5;
     string s = "Hallo";
 
+    int i1 = 2;
+
     Pair<int,string> p;
     cout << p << endl;
     Pair<int,string> p2 (i,s);
-
+    p = p2;
+    cout << "after p = p2 " << p << endl;
+    Pair<int,int> p1(i1,i);
+    //geht nicht schon der Compiler meckert
+//    p = p1;
+//    cout << "after p = p1 " << p << endl;
+    Pair<int,string> p3(p2);
+    cout << "after p3(p2) " << p3 << endl;
     //cout << p << endl;
 
    // cout << p.first();
--- Pair/pair.h
+++ Pair/pair.h
@@ -12,19 +12,28 @@ private:
 
 public:
 
-
-
     Pair(T1 t1=T1(), T2 t2=T2())
         :
             m_first(t1),
             m_second(t2)
     {}
+    //Copy Constructor
+    Pair(Pair<T1,T2>& p){
+        m_first = p.first();
+        m_second = p.second();
+    }
 
     T1& first() { return m_first; }
     T2& second(){ return m_second; }
 
     const T1& first() const { return m_first; }
     const T2& second() const { return m_second; }
+
+    Pair<T1,T2>& operator =(Pair<T1,T2>& p){
+        m_first = p.first();
+        m_second = p.second();
+        return *this;
+    }
 };
 
 template<typename T1, typename T2>
